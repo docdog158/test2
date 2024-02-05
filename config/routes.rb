84c148614ca86c53fbebecb91a71e9ab59bb2285
150042192+docdog158@users.noreply.github.com <ec2-user@ip-172-31-31-106.ap-northeast-1.compute.internal>
2024-02-05 12:45:24 +0000

    非同期通信

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,13 @@ Rails.application.routes.draw do
   	get "followers" => "relationships#followers", as: "followers"
   end
   
+  #検索機能
+  get "/search", to: "searches#search"
+
+  
+  
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
